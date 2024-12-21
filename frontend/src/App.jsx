35057a39b3e02c555e999fc81e065fd4c351eff6
Harsh <harshkumarshaw09@gamil.com>
2024-12-21 16:48:52 +0530

    Navbar completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/login";
+import About from "./pages/About";
+import Contact from "./pages/contact";
+import MyProfile from "./pages/MyProfile";
+import Myappointments from "./pages/Myappointments";
+import Appointments from "./pages/Appointments";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/doctors" element={<Doctors />}></Route>
+        <Route path="/doctors/:speciality" element={<Doctors />}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<Myappointments />} />
+        <Route path="/appointment/:docId" element={<Appointments />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
